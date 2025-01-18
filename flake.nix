a19642f3e4e5bd807e78e2f4cf40baf9e709b14b
dries <sander@aaadataplumbing.com>
2025-01-18 23:52:14 +0100

    migrate zsh to file

--- flake.nix
+++ flake.nix
@@ -83,56 +83,6 @@
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
-      programs.zsh = {
-        # ohMyZsh = {
-        #   enable = true;
-        #   plugins = [ "git" "sudo" "docker" "kubectl" ];
-        # };
-        enable = true;
-        interactiveShellInit = ''
-# First, ensure completion system is initialized
-autoload -Uz compinit
-compinit
-
-# Enable additional zsh settings that fzf-tab depends on
-zstyle ':completion:*' menu select
-zmodload zsh/complist
-
-# Source fzf completion and keybindings first
-if [[ -f ${pkgs.fzf}/share/fzf/completion.zsh ]]; then
-  source ${pkgs.fzf}/share/fzf/completion.zsh
-fi
-if [[ -f ${pkgs.fzf}/share/fzf/key-bindings.zsh ]]; then
-  source ${pkgs.fzf}/share/fzf/key-bindings.zsh
-fi
-
-# Then source fzf-tab
-if [[ -f ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh ]]; then
-  source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
-fi
-
-# Configure fzf behavior
-export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --border"
-export FZF_CTRL_R_OPTS="--sort --exact"
-export FZF_DEFAULT_COMMAND='rg --files'
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-
-bindkey "ç" fzf-cd-widget
-
-# Aliases
-alias lg="lazygit"
-alias ld="lazydocker"
-# alias air='~/.air'
-
-alias nosleep="sudo pmset -b disablesleep 1"
-alias yessleep="sudo pmset -b disablesleep 0"
-
-alias cu="cd .. && ll"
-ci () {
-	cd $1 && ll
-}
-        '';
-      };
       environment.shells = [ pkgs.zsh ];
 
 
@@ -149,6 +99,7 @@ ci () {
     darwinConfigurations."m4" = nix-darwin.lib.darwinSystem {
       modules = [
         ./modules/system.nix
+        ./modules/zsh.nix
 
         configuration
         nix-homebrew.darwinModules.nix-homebrew
